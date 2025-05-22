35a3f1d13970cccb0c2c49cad279b6919b5ba50e
Naveenjosh641 <bankapallinaveenjosh@gmail.com>
2025-05-22 11:39:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def evaluate_fit(data: FitRequest):
     }
 
 # Optional: dynamic port for local testing
-if _name_ == "_main_":
+if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
